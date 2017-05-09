e84424d4003f0329aa84041131defb9ab976b03a
Goudy <gowdilyan@gmail.com>
2017-05-09 19:43:18 -0400

    first commit

---
+++ src/main/java/demo/portal/Demo/App.java
@@ -0,0 +1,13 @@
+package demo.portal.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Ohio 43004!" );
+    }
+}
