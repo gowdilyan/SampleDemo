6374a8594da9fc964c104a89b131447235352533
Goudy <gowdilyan@gmail.com>
2017-05-09 21:04:20 -0400

    new commit - May 09 2103hrs

--- src/main/java/demo/portal/Demo/App.java
+++ src/main/java/demo/portal/Demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ohio 43004!" );
+        System.out.println( "Hello Ohio 4300!" );
     }
 }
